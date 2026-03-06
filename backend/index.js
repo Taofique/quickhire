5310e946d003f2852dc85c0e639a7a1e52649e1e
Taofique <shornob8@gmail.com>
2026-03-06 21:54:50 +0600

    chore: setup frontend with React, Vite, and Tailwind CSS

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,20 @@ import cors from "cors";
 import { connectDB } from "./db/connectDB.js";
 import jobRoutes from "./routes/jobs.route.js";
 import applicationRoutes from "./routes/applications.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
