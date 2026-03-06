ece66796388bfb272970e2c2a4c12f3f0e89b966
Taofique <shornob8@gmail.com>
2026-03-06 15:09:45 +0600

    feat: add express server setup with database connection

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./db/connectDB";
+import jobRoutes from "./routes/jobs.route.js";
+import applicationRoutes from "./routes/applications.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+
+app.use("/api/jobs", jobRoutes);
+app.use("/api/applications", applicationRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port:", PORT);
+});
