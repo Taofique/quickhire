89c79ba2df4016826743dcc4284cadd5ee0d3371
Taofique <shornob8@gmail.com>
2026-03-07 13:45:30 +0600

    fix: fix backend index.js static file serve, moving after API routes

--- backend/index.js
+++ backend/index.js
@@ -10,20 +10,20 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 const __dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("/{*splat}", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
 app.use("/api/jobs", jobRoutes);
 app.use("/api/applications", applicationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
